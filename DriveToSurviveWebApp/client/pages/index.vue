4dca80169664b60886b3ee5673d21c3ca55d66bc
4mpcln <krit.in@kkumail.com>
2026-02-18 01:02:45 +0700

    ค้นหาเส้นทาง done

--- DriveToSurviveWebApp/client/pages/index.vue
+++ DriveToSurviveWebApp/client/pages/index.vue
@@ -198,7 +198,7 @@
     <!-- CTA -->
     <section class="pt-10 pb-55 bg-slate-100 sm:pt-24 lg:pt-28">
       <div class="px-6 mx-auto max-w-4xl sm:px-8 lg:px-12 text-center">
-        <h2 class="text-5xl font-bold leading-tight tracking-tight text-[#137FEC] font-heading sm:text-6xl lg:text-7xl mb-6">
+        <h2 class="text-3xl font-semibold text-[#137FEC] font-heading sm:text-5xl lg:text-7xl mb-4">
           พร้อมเริ่มต้นแล้วหรือยัง?
         </h2>
         <p class="text-lg text-slate-600 mb-10 max-w-2xl mx-auto">
@@ -221,45 +221,8 @@
     </section>
 
     <!-- Footer -->
-    <footer class="py-8 bg-white border-t border-slate-200">
-      <div class="px-6 mx-auto max-w-7xl sm:px-8 lg:px-12">
-        <div class="grid gap-12 md:grid-cols-4">
-          <div class="md:col-span-2">
-            <div class="flex items-center gap-3 mb-6">
-              <div class="flex items-center justify-center w-10 h-10 rounded-lg ">
-                <img src="/Ride.png" alt="Ride Logo" class="h-8 w-auto object-contain" />
-              </div>
-              <span class="text-3xl font-bold text-[#236993] font-heading">Ride</span>
-            </div>
-            <p class="text-slate-600 max-w-md mb-6 leading-relaxed font-semibold">
-              แพลตฟอร์มแชร์รถที่ปลอดภัยและน่าเชื่อถือ<br>
-              ด้วยระบบยืนยันตัวตน eKYC และการติดตาม GPS แบบเรียลไทม์
-            </p>
-          </div>
-          
-          <div>
-            <h4 class="mb-4 font-bold text-slate-600 text-lg font-heading">ลิงก์สำคัญ</h4>
-            <div class="space-y-3">
-              <NuxtLink :to="localePath('/findTrip')" class="block text-[#236993] hover:text-[#137FEC] transition-colors font-medium ml-3">ค้นหาเส้นทาง</NuxtLink>
-              <NuxtLink :to="localePath('/createTrip')" class="block text-[#236993] hover:text-[#137FEC] transition-colors font-medium ml-3">สร้างเส้นทาง</NuxtLink>
-              <NuxtLink :to="localePath('/about')" class="block text-[#236993] hover:text-[#137FEC] transition-colors font-medium ml-3">เกี่ยวกับเรา</NuxtLink>
-            </div>
-          </div>
-          
-          <div>
-            <h4 class="mb-4 font-bold text-slate-600 text-lg font-heading">นโยบาย</h4>
-            <div class="space-y-3">
-              <NuxtLink :to="localePath('/terms-of-service')" class="block text-[#236993] hover:text-[#137FEC] transition-colors font-medium ml-3">ข้อกำหนดการใช้งาน</NuxtLink>
-              <NuxtLink :to="localePath('/privacy')" class="block text-[#236993] hover:text-[#137FEC] transition-colors font-medium ml-3">นโยบายความเป็นส่วนตัว</NuxtLink>
-            </div>
-          </div>
-        </div>
-        
-        <div class="pt-20 mt-15 text-center">
-          <p class="text-[#236993] text-sm font-medium">© {{ new Date().getFullYear() }} Ride | สงวนลิขสิทธิ์</p>
-        </div>
-      </div>
-    </footer>
+    <!-- Footer -->
+    <AppFooter />
   </div>
 </template>
 
